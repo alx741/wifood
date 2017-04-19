2f198b5137617a7ec62c25f943ccb3d4cabf2add
Daniel Campoverde [alx741] <alx@sillybytes.net>
2017-04-18 22:15:58 -0500

    Add default menu items

--- wifood/Handler/Table.hs
+++ wifood/Handler/Table.hs
@@ -46,4 +46,108 @@ getPopulateR = do
 
         items :: [Item]
         items =
-            [ Item "Churrasco" "Carne de res, arroz, porción de papas, huevo frito y ensalada." 4.0 Main]
+            [ Item
+                "Churrasco"
+                "Carne de res, arroz, porción de papas, huevo frito y ensalada."
+                4.0 Main
+
+            , Item
+                "Alitas a la barbiquiu"
+                "Alitas bañadas en salsa barbiquiu, porción de papas y ensalada."
+                3.75 Main
+
+            , Item
+                "Pollo al horno con papas"
+                "Pollo, porción de papas, ensalada y aderezo de salsas."
+                3.50 Main
+
+            , Item
+                "Pescado frito con papas"
+                "Filete de pescado, porción de papas, ensalada y aderezo de salsas."
+                4.50 Main
+
+            , Item
+                "Lasaña"
+                "Porción de lasaña con ensalada"
+                3.00 Main
+
+            , Item
+                "Parrillada"
+                "Carne de res, pollo, carne de cerdo, porción de papas fritas, papas cocidas y ensalada."
+                10.00 Main
+
+            , Item
+                "Chaulafán"
+                "Chaulafán con camarón."
+                3.00 Main
+
+            , Item
+                "Lomo a la plancha"
+                "Lomo de res, ensalada y aderezo de salsas."
+                4.00 Main
+
+            , Item
+                "Cangrejada"
+                "Cangrejo, ensalada, guacamole, porción de chifles y aderezo de salsas."
+                7.00 Main
+
+            , Item
+                "Cuy con papas"
+                "Cuy, porción de papas cocidas, porción de arroz, mote, huevo duro y ensalada."
+                7.50 Main
+
+            , Item
+                "Ceviche de camarón"
+                "Ceviche de camarón, porción de chifles y canguil."
+                5.00 Main
+
+            , Item
+                "Porción  tres leches"
+                "Porción bañado en tres leches con aderezos."
+                3.50 Dessert
+
+            , Item
+                "Brownie de chocolate"
+                "Brownie con nueces bañado en chocolate."
+                2.50 Dessert
+
+            , Item
+                "Crepes con nutella"
+                "Crepes rellenas con nutella y durazno."
+                2.00 Dessert
+
+            , Item
+                "Brochetas de frutas con chocolate"
+                "Brochetas de guineo, frutilla,uvas,kiwi y manzana bañadas en chocolate."
+                3.50 Dessert
+
+            , Item
+                "Porción de torta con helado"
+                "Porción de torta bañada en chocolate y helado a su gusto(mora, vainilla, chocolate, chicle)"
+                4.00 Dessert
+
+            , Item
+                "Cheesecake de mora"
+                "Cheesecake con chispas de mora."
+                6.00 Dessert
+
+            , Item
+                "Capuchino"
+                ""
+                2.00 Drink
+
+            , Item
+                "Cóctel"
+                ""
+                2.50 Drink
+
+            , Item
+                "Café Express"
+                ""
+                1.50 Drink
+
+            , Item
+                "Jugos naturales"
+                "Jugos sabor a mora, naranja y guanábana."
+                1.00 Drink
+            ]
